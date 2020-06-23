85e1cf0029c318dccad0c472d8461499f0f3b612
Martin Fenner <martin.fenner@datacite.org>
2020-06-23 06:43:36 +0200

    remove readingtime script

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,7 @@ const plugins = require('./webpack/plugins.js');
 module.exports = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   entry: {
-    index: './source/javascripts/index.js',
     search: './source/javascripts/search.js',
-    readingTime: './source/javascripts/readingTime.min.js',
   },
   module: {
     rules: [
